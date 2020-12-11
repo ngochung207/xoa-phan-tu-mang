94f1c8d0a5e0daced1ad97a1a3ae1550a5585db0
ngochung207 <ngochung207@gmail.com>
2020-12-12 04:49:56 +0700

    update code

---
+++ src/tim_gia_tri_lon_nhat_mang_2_chieu.java
@@ -0,0 +1,18 @@
+public class tim_gia_tri_lon_nhat_mang_2_chieu {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {0,1,2,3,4,5,6},
+                {7,8,9,10,11,12,13},
+        };
+        int valueMax = 0;
+        for (int[] x: arr
+             ) {
+            for(int i = 0; i < x.length; i++){
+                if (valueMax < x[i]){
+                    valueMax = x[i];
+                }
+            }
+        };
+        System.out.println(valueMax);
+    }
+}
