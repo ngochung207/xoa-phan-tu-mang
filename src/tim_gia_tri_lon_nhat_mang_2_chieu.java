690143eba93a0398a995ba1a433128405eeb2f95
ngochung207 <ngochung207@gmail.com>
2020-12-12 04:53:33 +0700

    udpate code

--- src/tim_gia_tri_lon_nhat_mang_2_chieu.java
+++ src/tim_gia_tri_lon_nhat_mang_2_chieu.java
@@ -4,15 +4,19 @@ public class tim_gia_tri_lon_nhat_mang_2_chieu {
                 {0,1,2,3,4,5,6},
                 {7,8,9,10,11,12,13},
         };
-        int valueMax = 0;
+        int valueMax = arr[0][0];
+        int valueMin = arr[0][0];
         for (int[] x: arr
              ) {
             for(int i = 0; i < x.length; i++){
                 if (valueMax < x[i]){
                     valueMax = x[i];
+                } else if (valueMin> x[i]){
+                    valueMin = x[i];
                 }
             }
         };
         System.out.println(valueMax);
+        System.out.println(valueMin);
     }
 }
